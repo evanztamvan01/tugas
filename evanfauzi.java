392a93493833d84144222836cbf12ef0318562a4
Evan Fauzi <40778475+evanztamvan01@users.noreply.github.com>
2019-03-07 10:49:06 +0700

    Add files via upload

---
+++ evanfauzi.java
@@ -0,0 +1,7 @@
+public class evanfauzi {
+    public static void main(String[] args) {
+        System.out.println("Nama  : Evan Fauzi Pranendra\n"
+                         + "NIM   : 17090041\n"
+                         + "Kelas : TI 4A");
+    }
+}
